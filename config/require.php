12ed96f01913cf780259abb7280228952517a519
fntsrlike <fntsrlike@gmail.com>
2013-10-20 05:00:31 +0800

    add file to require all files

---
+++ config/require.php
@@ -0,0 +1,14 @@
+<?php
+
+    // config
+    require("config/db_const.php");
+
+    // liberies
+    require("liberies/o_user.php");
+    require("liberies/o_student.php");
+
+    // models
+    require("models/i_crud.php");
+    require("models/user_model.php");
+    require("models/group_model.php");
+    require("models/identidy.php");
