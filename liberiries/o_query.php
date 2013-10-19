0ddb76f30b10328bdaf3c23995d5d55fc757ee55
fntsrlike <fntsrlike@gmail.com>
2013-10-20 05:47:09 +0800

    initial class OQuery

---
+++ liberiries/o_query.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* 
+*/
+class OQuery
+{
+    private $query  = null;
+    private $row_no = 0;
+
+    public function __construct($query)
+    {
+        $this->query = $query;
+    }
+
+    public function row() {
+
+    }
+
+    public function row_arr() {
+
+    }
+
+    public function result() {
+
+    }
+
+    public function result_arr() {
+
+    }
+
+    public function num_rows() {
+
+    }
+
+    public function num_fields() {
+
+    }
+
+}
