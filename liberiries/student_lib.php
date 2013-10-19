7144ed67c3a529e45c7c7a98fd85b1cdbeb319c9
fntsrlike <fntsrlike@gmail.com>
2013-10-20 04:52:15 +0800

    add OStudent.

---
+++ liberiries/student_lib.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* 
+*/
+class OStudent extends OUser
+{
+    private $sId    = null;
+    private $depart = null;
+    private $garde  = null;
+    
+    public function __construct($user_code = null) {
+        parent::__construct($user_code);
+    }
+
+    public function get_sid() {
+
+    }
+
+    public function set_sid() {
+
+    }
+
+    public function get_dept() {
+
+    }
+
+    public function set_dept() {
+
+    }
+
+    public function get_grade() {
+
+    }
+
+    public function set_grade() {
+
+    }    
+}
--- liberiries/user_lib.php
+++ liberiries/user_lib.php
@@ -2,7 +2,7 @@
 /**
 * 
 */
-class UserLib
+class OUser
 {
     private $id   = null;
     private $username   = null;
