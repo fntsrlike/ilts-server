a8aa26ba3224bf44cda9cece4f6023a8bd68c9fd
fntsrlike <fntsrlike@gmail.com>
2013-10-30 03:15:29 +0800

    增加權限的清單功能:

--- application/controllers/identify.php
+++ application/controllers/identify.php
@@ -10,15 +10,15 @@ class Identify extends CI_Controller {
     public function list_all()
     {
         $this->load->model('identify_model');
-        $users = $this->user_model->list_all();
+        $users = $this->identify_model->list_all();
 
         $this->load->library('table');
         $table = array();
         $this->table->set_template(array('table_open' => '<table class="table"'));
-        $this->table->set_heading('Identify id', 'User', 'Organization', 'Level', 'Register Time');
+        $this->table->set_heading('Identify id', 'User', 'Organization', 'Level', 'Created Time');
 
         foreach ($users as $row) {
-            $table[] = array($row->id, $row->name, $row->status, $row->created, $provider);
+            $table[] = array($row->id, $row->user, $row->org, $row->level, $row->created);
         }
 
         $data['table'] = $this->table->generate($table);;
---
+++ application/models/identify_model.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Identify_model extends CI_Model {
+
+    public function list_all()
+    {
+        $this->load->model('portal_model');
+        $this->load->model('organization_model');
+
+        $this->db->order_by('iId','asc');
+        $result = $this->db->get('identify_tag')->result();
+        $user_obj_arr = array();
+
+        foreach ($result as $row) {
+             $user = new ArrayObject(array(), ArrayObject::STD_PROP_LIST);
+
+             $user->id      = $row->iId;
+             $user->user    = $this->portal_model->read_user($row->uId)->uName  . " ({$row->uId})";
+             $user->org     = $this->organization_model->read_organ($row->oId)->oName . " ({$row->oId})";
+             $user->level   = $row->iLevel;
+             $user->status  = $row->iStatus;
+             $user->created = $row->iCreateTime;
+
+             $user_obj_arr[] = $user;
+        }
+
+        return $user_obj_arr;        
+
+    }    
+
+}
+
+/* End of file identify_model.php */
+/* Location: ./application/models/identify_model.php */
