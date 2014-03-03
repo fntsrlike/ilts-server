77169510f9b2c48ffc4709d96b9709bd67920840
Ruoshi.Ling <fntsrlike@gmail.com>
2014-03-03 23:01:48 +0800

    新增管理員與OAuth Provider專用的控制器，準備將伊爾特系統Codeigniter版本的程序移植過來

---
+++ app/controllers/OAuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+class OAuthController extends BaseController {
+
+    protected $layout = 'master';
+
+    public function index()
+    {
+        return 'admin index';
+    }
+
+    public function author_sever()
+    {
+
+    }
+
+    public function resource_owner()
+    {
+
+    }
+
+    public function resource_sever()
+    {
+
+    }
+
+    public function client_tester()
+    {
+
+    }
+
+    public function header()
+    {
+
+    }
+
+
+}
