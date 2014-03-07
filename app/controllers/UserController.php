7f3ff4ac78997e99f1a36aa2e0d1edfa34ae5bf1
Ruoshi.Ling <fntsrlike@gmail.com>
2014-03-07 16:05:07 +0800

    完成使用者專區基本版型

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,11 +2,16 @@
 
 class UserController extends BaseController {
 
-    protected $layout = 'user.master';
+    protected $layout = 'master';
 
     public function info()
     {
-        $data['user'] = (object) Session::get('user_being');
+        $user = IltUser::find(Session::get('user_being.u_id'));
+        $user_option = IltUserOptions::find(Session::get('user_being.u_id'));
+
+        $data['provider']    = Session::get('user_being.provider');
+        $data['user']        = $user;
+        $data['user_option'] = $user_option;
         return View::make('user/info', array('name' => 'user'))->with($data);
     }
 
